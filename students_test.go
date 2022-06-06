4917747586775f1a869a7363d2089dd41b03af09
Khrystyna2409 <xrustuna.di@gmail.com>
2022-06-06 14:42:26 +0300

    1

--- students_test.go
+++ students_test.go
@@ -1,7 +1,9 @@
 package coverage
 
 import (
+	"testing"
 	"os"
+	"time"
 )
 
 // DO NOT EDIT THIS FUNCTION
@@ -17,4 +19,21 @@ func init() {
 }
 
 // WRITE YOUR CODE BELOW
+
+//per1 := Person{firstName: "Anna", lastName: "Grad", birthDay: time.Date(2002, 3, 4, 10, 30, 0, 0, time.UTC)}
+//Per2:= Person{firstName: "Bohdan", lastName: "Horvat", birthDay: time.Date(1991, 3, 4, 10, 30, 0, 0, time.UTC)}
+//Per3 := Person{firstName: "Vika", lastName: "Lestless", birthDay: time.Date(1991, 3, 4, 10, 30, 0, 0, time.UTC)}
+//All := People{Per1, Per2, Per3}
+
+func TestLenOK(t *testing.T){
+	var all People
+	Per1 := Person{firstName: "Anna", lastName: "Grad", birthDay: time.Date(2002, 3, 4, 10, 30, 0, 0, time.UTC)}
+	Per2:= Person{firstName: "Bohdan", lastName: "Horvat", birthDay: time.Date(1991, 3, 4, 10, 30, 0, 0, time.UTC)}
+	all = append(all, Per1, Per2)
+	res := all.Len()
+	if res != 2 {
+		t.Errorf("Please pay attention to input")
+	}
+}
+
 
